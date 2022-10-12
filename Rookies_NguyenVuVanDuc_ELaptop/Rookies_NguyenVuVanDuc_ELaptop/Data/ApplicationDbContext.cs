cd313fc335e1a461db99359ccc7595d9c02c2f66
ducvuive <duc20062001@gmail.com>
2022-10-12 09:14:23 +0700

    Create project
    Create project LaptopStore with template ASP.NET Core MVC

---
+++ Rookies_NguyenVuVanDuc_ELaptop/Rookies_NguyenVuVanDuc_ELaptop/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rookies_NguyenVuVanDuc_ELaptop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
